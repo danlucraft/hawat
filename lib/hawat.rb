091954f06e3ac261d4a7cce7c5ef4c977490d5d3
Daniel Lucraft <dan@fluentradical.com>
2013-04-28 11:40:09 +0100

    Add max concurrency statistic

--- lib/hawat.rb
+++ lib/hawat.rb
@@ -4,9 +4,6 @@ class Hawat
     @log_path = log_path
   end
 
-  # example 
-  # Apr 28 00:00:15 dc1-live-lb1.srv.songkick.net haproxy[32647]: 10.32.75.139:53757 [28/Apr/2013:00:00:15.885] skweb skweb/dc1-live-frontend6_3000 0/0/9/9/20 200 5732 - - ---- 104/39/39/5/0 0/0 \"GET /favicon.ico HTTP/1.1\"\n"
-  LINE_RE = /^(\w+ \d+ \d+:\d+:\d+) (\S+) haproxy\[(\d+)\]: ([\d\.]+):(\d+) \[(\d+)\/(\w+)\/(\d+):(\d+:\d+:\d+\.\d+)\] ([^ ]+) ([^ ]+)\/([^ ]+) (-?\d+)\/(-?\d+)\/(-?\d+)\/(-?\d+)\/(-?\d+) (\d+) (\d+) - - ([\w-]+) (-?\d+)\/(-?\d+)\/(-?\d+)\/(-?\d+)\/(-?\d+) (\d+)\/(\d+) "(\w+) ([^ ]+)/
 
   class NamedAggregate
     def initialize(name, aggregate)
@@ -22,12 +19,32 @@ class Hawat
     end
   end
 
+  class Aggregate
+    def initialize
+      @stats = Statistics.new
+      @conc = Concurrency.new
+    end
+
+    def update(line)
+      @stats.update(line)
+      @conc.update(line)
+    end
+    
+    def result
+      {
+        "stats" => @stats.result,
+        "concurrency" => @conc.result
+      }
+    end
+  end
+
   class Statistics
     def initialize
       @count = 0
       @total_duration = 0
       @max_duration = 0
       @min_duration = 100000000
+      @errors = 0
     end
 
     def update(line_data)
@@ -36,16 +53,41 @@ class Hawat
       @total_duration += dur
       @max_duration = dur if dur > @max_duration
       @min_duration = dur if dur < @max_duration
+      @errors += 1 if line_data.status =~ /^(4|5)/ and line_data.status != "404"
     end
 
     def result
       {
         "count" => @count,
+        "errors" => @errors,
         "duration" => {
           "min"  => @min_duration,
-          "mean" => (@duration.to_f/@count).to_i,
+          "mean" => (@total_duration.to_f/@count).to_i,
           "max"  => @max_duration
         }
+      }
+    end
+  end
+
+  class Concurrency
+    def initialize
+      @max_concurrency = 0
+      @live_requests = []
+    end
+
+    def update(line)
+      acc_s = line.accepted.to_r
+      dur_s = Rational(line.total_time, 1000)
+      close = acc_s + dur_s
+      @live_requests.reject! {|l| l < close }
+      @live_requests << close
+      conc = @live_requests.length
+      @max_concurrency = conc if conc > @max_concurrency
+    end
+
+    def result
+      {
+        "max_concurrency" => @max_concurrency
       }
     end
   end
@@ -53,7 +95,7 @@ class Hawat
   class StatisticsCollection < Hash
     def initialize
       super
-      self.default_proc = lambda {|h,k| h[k] = Statistics.new }
+      self.default_proc = lambda {|h,k| h[k] = Aggregate.new }
     end
 
     def result
@@ -63,7 +105,6 @@ class Hawat
       end
       h
     end
-
   end
 
   class FrontendStatistics
@@ -82,7 +123,7 @@ class Hawat
 
   def default_stats
     [
-      NamedAggregate.new("global", Statistics.new),
+      NamedAggregate.new("global", Aggregate.new),
       FrontendStatistics.new
     ]
   end
@@ -109,6 +150,10 @@ class Hawat
     stats.map(&:result).inject(&:merge)
   end
 
+  # example 
+  # Apr 28 00:00:15 dc1-live-lb1.srv.songkick.net haproxy[32647]: 10.32.75.139:53757 [28/Apr/2013:00:00:15.885] skweb skweb/dc1-live-frontend6_3000 0/0/9/9/20 200 5732 - - ---- 104/39/39/5/0 0/0 \"GET /favicon.ico HTTP/1.1\"\n"
+  LINE_RE = /^(\w+ \d+ \d+:\d+:\d+) (\S+) haproxy\[(\d+)\]: ([\d\.]+):(\d+) \[(\d+)\/(\w+)\/(\d+):(\d+:\d+:\d+\.\d+)\] ([^ ]+) ([^ ]+)\/([^ ]+) (-?\d+)\/(-?\d+)\/(-?\d+)\/(-?\d+)\/(-?\d+) (\d+) (\d+) - - ([\w-]+) (-?\d+)\/(-?\d+)\/(-?\d+)\/(-?\d+)\/(-?\d+) (\d+)\/(\d+) "(\w+) ([^ ]+)/
+
   class LineData
     attr_accessor :md
 
@@ -116,28 +161,40 @@ class Hawat
     def haproxy_pid;  md[3]; end
     def haproxy_ip;  md[4]; end
     def haproxy_port;  md[5]; end
+
     def day;           md[6]; end
     def month; md[7]; end
     def year; md[8]; end
     def timestamp; md[9]; end
+
+    def accepted
+      hour, minute, sec, milli = *timestamp.split(/:|\./)
+      Time.local(year.to_i, month, day.to_i, hour.to_i, minute.to_i, sec.to_i, milli.to_i*1000)
+    end
+
     def frontend; md[10]; end
     def backend; md[11]; end
     def server; md[12]; end
+
     def tq; md[13].to_i; end
     def tw; md[14].to_i; end
     def tc; md[15].to_i; end
     def tr; md[16].to_i; end
     def total_time; md[17].to_i; end
+
     def status; md[18]; end
     def bytes; md[19]; end
     def termination_state; md[20]; end
+
     def actconn; md[21]; end
     def feconn; md[22]; end
     def beconn; md[23]; end
     def srv_conn; md[24]; end
     def retries; md[25]; end
+
     def srv_queue; md[26]; end
     def backend_queue; md[27]; end
+
     def http_method; md[28]; end
     def path; md[29]; end
   end
